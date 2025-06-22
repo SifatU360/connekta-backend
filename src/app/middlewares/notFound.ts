bef4ae4f0b6444ad12da35fcacf14f105e5d513d
SifatU360 <sifatullah30.info@gmail.com>
2025-06-22 23:05:05 +0600

    Backend Initialization Done Twice

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -7,7 +7,7 @@ import httpStatus from 'http-status';
 const notFound = (req: Request, res: Response, next: NextFunction) => {
     res.status(httpStatus.NOT_FOUND).json({
     success: false,
-    message: 'API Not Found !!',
+    message: 'API is Not Found !!!!',
     error: '',
   });
 };
