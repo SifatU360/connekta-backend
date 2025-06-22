ba1f19a29772f4fdd59a71b676690f3331e74b27
SifatU360 <sifatullah30.info@gmail.com>
2025-06-22 23:00:48 +0600

    Backend Initialization Done

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hi Next Level Developer !');
+  res.send('Hi Connekta User !');
 });
 
 app.use(globalErrorHandler);
